a7be413205770fac5886c1211c027293a22c33bd
0hyoon0 <gpdbs0979@naver.com>
2024-05-06 13:28:34 +0900

    first

---
+++ spi_sender/spi_sender.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600); 
+  while (!Serial);
+
+  Serial.println("LoRa Sender");
+
+  if (!LoRa.begin(9209E5)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("10123456789,0123456789,0123456789,0123456789,0123456789,0123456789,0123456789,0123456789,0123456789,0123456789,0123456789");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+
+  delay(100);
+}
