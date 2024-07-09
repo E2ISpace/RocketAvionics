1e843b305904e983629fea831255c0861833d064
park-chu <parkchuy7077@kyonggi.ac.kr>
2024-07-09 15:01:37 +0900

    시리얼에서 리셋 가능하게 변경

--- remote_reboot/ground_reboot_systeam.ino
+++ remote_reboot/ground_reboot_systeam.ino
@@ -5,8 +5,8 @@ const int csPin = 10;    // LoRa 모듈 CS 핀
 const int resetPin = 9;  // LoRa 모듈 리셋 핀
 const int irqPin = 2;    // LoRa 모듈 IRQ 핀
 
-const int reset_buttonpin = 5; // 리셋 버튼 핀
-int flag = 0; // 플래그 변수
+//const int reset_buttonpin = 5; // 리셋 버튼 핀
+//int flag = 0; // 플래그 변수
 
 void setup() {
   Serial.begin(9600);
@@ -20,25 +20,22 @@ void setup() {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-
-  pinMode(reset_buttonpin, INPUT); // 리셋 버튼 핀을 입력 모드로 설정
 }
 
 void loop() {
-  // 리셋 버튼이 눌렸을 때 "REBOOT" 메시지를 전송
-  if (digitalRead(reset_buttonpin) == HIGH) {
-    if (flag == 0) {
-      flag = 1; // 플래그 설정
-      Serial.println("Sending REBOOT...");
+ if (Serial.available() > 0) { // 시리얼 입력이 있는지 확인
+    String input = Serial.readStringUntil('\n'); // 개행 문자까지 문자열을 읽음
 
-      LoRa.beginPacket();
-      LoRa.print("REBOOT");
-      LoRa.endPacket();
+    // 입력된 문자열이 "reset"인지 확인
+    if (input.equals("reboot")) {
+        Serial.println("Sending reboot signal...");
+        LoRa.beginPacket();
+        LoRa.print("REBOOT");
+        LoRa.endPacket();
+    } else {
+      Serial.println("Unknown command. Type 'reboot' to reboot Arduino.");
     }
-  } else {
-    flag = 0; // 버튼이 눌리지 않으면 플래그 초기화
   }
-
   delay(500); // 버튼 입력 확인 주기 (500ms)
 
   // 메시지 수신 모드로 전환
