0a31fe74087b7c8d14a747b3369e655ba12a90d3
park-chu <parkchuy7077@kyonggi.ac.kr>
2024-07-11 13:04:26 +0900

    로켓 파일 정리

--- remote_reboot/ground_reboot_systeam.ino
+++ remote_reboot/ground_reboot_systeam.ino
@@ -5,9 +5,6 @@ const int csPin = 10;    // LoRa 모듈 CS 핀
 const int resetPin = 9;  // LoRa 모듈 리셋 핀
 const int irqPin = 2;    // LoRa 모듈 IRQ 핀
 
-//const int reset_buttonpin = 5; // 리셋 버튼 핀
-//int flag = 0; // 플래그 변수
-
 void setup() {
   Serial.begin(9600);
   while (!Serial);
@@ -16,35 +13,36 @@ void setup() {
 
   // LoRa 모듈 초기화
   LoRa.setPins(csPin, resetPin, irqPin);
-  if (!LoRa.begin(433E6)) {
+  if (!LoRa.begin(920E6)) { // 920.9 MHz -> 920E6 (920 MHz)
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-}
-
-void loop() {
- if (Serial.available() > 0) { // 시리얼 입력이 있는지 확인
-    String input = Serial.readStringUntil('\n'); // 개행 문자까지 문자열을 읽음
-
-    // 입력된 문자열이 "reset"인지 확인
-    if (input.equals("reboot")) {
-        Serial.println("Sending reboot signal...");
-        LoRa.beginPacket();
-        LoRa.print("REBOOT");
-        LoRa.endPacket();
-    } else {
-      Serial.println("Unknown command. Type 'reboot' to reboot Arduino.");
-    }
-  }
-  delay(500); // 버튼 입력 확인 주기 (500ms)
 
   // 메시지 수신 모드로 전환
   LoRa.onReceive(onReceive);
   LoRa.receive();
 }
 
+void loop() {
+  if (Serial.available() > 0) { // 시리얼 입력이 있는지 확인
+    String input = Serial.readStringUntil('\n'); // 개행 문자까지 문자열을 읽음
+    input.trim(); // 입력 문자열의 앞뒤 공백 제거
+
+    // 입력된 문자열이 "reboot"인지 확인
+    if (input.equals("reboot")) {
+      Serial.println("Sending reboot signal...");
+      LoRa.beginPacket();
+      LoRa.print("REBOOT");
+      LoRa.endPacket();
+    } else {
+      Serial.println("Unknown command. Type 'reboot' to reboot Arduino.");
+    }
+  }
+  delay(500); // 버튼 입력 확인 주기 (500ms)
+}
+
 void onReceive(int packetSize) {
-  if (packetSize == 0) return; 
+  if (packetSize == 0) return;
 
   String incoming = "";
   while (LoRa.available()) {
