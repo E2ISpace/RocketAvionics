f82a01f5a5cbb150a8efca89ffb58604b708d6cd
Park <parkchuy7077@gmail.com>
2024-08-08 19:49:41 +0900

    spi_reciver에 reboot 적용

--- receiver+data_seperate.ino
+++ receiver+data_seperate.ino
@@ -7,10 +7,10 @@ const int resetPin = 9;  // LoRa 모듈 리셋 핀
 const int irqPin = 2;    // LoRa 모듈 IRQ 핀
 const long frequency = 9209E5; // 주파수 설정
 
-LoRaReboot receiver(csPin, resetPin, irqPin);
+LoRareboot receiver(csPin, resetPin, irqPin);
 
 void setup() {
-  receiver.begin(frequency);
+  receiver.begin(9209E5);
 }
 
 void loop() {
@@ -38,6 +38,8 @@ void loop() {
       values[index++] = atof(ptr); // 문자열을 실수형(float)으로 변환하여 배열에 저장
       ptr = strtok(NULL, ",");
     }
+
+    // yaw, pitch, roll 값을 시리얼 포트로 전송
     Serial.print(values[0]);
     Serial.print(",");
     Serial.print(values[1]);
