a6c90915ce898f8e669f6a31b9d3e858b3d6a655
AutoCorrelation <minjun.chung@gmail.com>
2024-08-11 03:51:38 +0900

    상보필터 쓰지마세요;

--- NewRocket/NewRocket.ino
+++ NewRocket/NewRocket.ino
@@ -2,6 +2,7 @@
 #include "MPU.h"
 #include <SD.h>
 #include <SPI.h>
+#include <MsTimer2.h>
 
 // 데이터 타입을 효율적으로 사용
 const byte servoPin = 3;
@@ -11,30 +12,41 @@ MPU9250 mpu;
 Servo servo;
 File dataFile;
 
-float angle = 0.0; // 전역 변수로 선언하여 AngleCalculator에서 사용
-unsigned long startTime; // 타이머를 위한 변수
+float angle = 0.0;           // 전역 변수로 선언하여 AngleCalculator에서 사용
+unsigned long prevTime;      // 타이머를 위한 변수
 unsigned long loopStartTime; // 타이머를 위한 변수
-unsigned long timeWhen; // 타이머를 위한 변수
-unsigned long dt; // 타이머를 위한 변수
+float timeWhen;              // 타이머를 위한 변수
+const unsigned long dt = 20; // 0.02 s=20ms
+float acc_Pitch, gyro_Pitch = 0;
+float acc_Roll, gyro_Roll = 0;
+float gyro_Yaw = 0;
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float filter_Pitch, filter_Roll;
 
-bool isAngleCheckEnabled = false; // angle 체크 활성화 여부
-
-void setup() {
+void setup()
+{
     Serial.begin(9600);
     Wire.begin();
     servo.attach(servoPin);
     servo.write(0);
     delay(1000);
 
-    if (!SD.begin(chipSelect)) {
-      while (1); // 초기화 실패 시 무한 루프
+    if (!SD.begin(chipSelect))
+    {
+        while (1)
+            ; // 초기화 실패 시 무한 루프
     }
-    
+
     dataFile = SD.open("data.txt", FILE_WRITE);
-    if (dataFile) {
-        dataFile.println(F("Time,AccX,AccY,AccZ,GyroX,GyroY,GyroZ,Yaw,Pitch,Roll,Angle"));
-    } else {
-        while (1); // 파일 열기 실패 시 무한 루프
+    if (dataFile)
+    {
+        dataFile.println("Time,AccX,AccY,AccZ,GyroX,GyroY,GyroZ,Angle");
+    }
+    else
+    {
+        while (1)
+            ; // 파일 열기 실패 시 무한 루프
     }
 
     // MPU9250 초기 설정 최소화
@@ -48,68 +60,102 @@ void setup() {
     setting.accel_fchoice = 0x01;
     setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
 
-    if (!mpu.setup(0x68, setting)) {
-      while (1); // MPU9250 초기화 실패 시 무한 루프
+    if (!mpu.setup(0x68, setting))
+    {
+        while (1)
+            ; // MPU9250 초기화 실패 시 무한 루프
     }
 
     mpu.setAccBias(-946.44, 633.1475, -1104.61);
     mpu.setGyroBias(280.12, -488.685, 167.025);
+    // startTime = millis(); // 설정 시작 시간을 기록
+    // isAngleCheckEnabled = false; // 설정 완료 전까지 angle 체크 비활성화
 
-    startTime = millis(); // 설정 시작 시간을 기록
-    isAngleCheckEnabled = false; // 설정 완료 전까지 angle 체크 비활성화
+    MsTimer2::set(13000, deployParachute);
+    MsTimer2::start();
 }
 
-void loop() {
-    loopStartTime=micros();
-    if (mpu.update()) {
-        float yaw = mpu.getYaw();
-        float pitch = mpu.getPitch();
-        float roll = mpu.getRoll();
-        float AccX = mpu.getAccX();
-        float AccY = mpu.getAccY();
-        float AccZ = mpu.getAccZ(); //data Value 추가하기
-        timeWhen = startTime-millis();
+void loop()
+{
+    loopStartTime = millis();
+    if (loopStartTime - prevTime >= dt)
+    {
+        if (mpu.update())
+        {
+            // yaw = mpu.getYaw();
+            // pitch = mpu.getPitch();
+            // roll = mpu.getRoll();
+            AccX = mpu.getAccX();
+            AccY = mpu.getAccY();
+            AccZ = mpu.getAccZ();
+            GyroX = mpu.getGyroX();
+            GyroY = mpu.getGyroY();
+            GyroZ = mpu.getGyroZ();        // data Value 추가하기
+            timeWhen = millis() / 1000.0f; // 시간 저장하기
+            filter(AccX, AccY, AccZ, GyroX, GyroY, GyroZ);
 
-        // Angle계산
-        AngleCalculator(yaw, pitch, roll);
+            // Angle계산
+            // AngleCalculator(yaw, pitch, roll);
 
-        if (dataFile) {
-            dataFile.print(AccX);
-            dataFile.print(", ");
-            dataFile.print(AccY);
-            dataFile.print(", ");
-            dataFile.print(AccZ);
-            dataFile.print(", ");
-            dataFile.print(yaw);
-            dataFile.print(", ");
-            dataFile.print(pitch);
-            dataFile.print(", ");
-            dataFile.print(roll);
-            dataFile.print(", ");
-            dataFile.println(angle);
-            dataFile.flush(); // 데이터가 즉시 쓰이도록 보장 
-        } 
+            if (dataFile)
+            {
+                dataFile.print(timeWhen - 1.59f);
+                Serial.print("filter_Pitch:"); // check complete
+                Serial.println(filter_Pitch); // check complete
+                Serial.print("filter_Roll:");  // check complete
+                Serial.println(filter_Roll);  // check complete
+                dataFile.print(", ");
+                dataFile.print(AccX);
+                dataFile.print(", ");
+                dataFile.print(AccY);
+                dataFile.print(", ");
+                dataFile.print(AccZ);
+                dataFile.print(", ");
+                dataFile.print(GyroX);
+                dataFile.print(", ");
+                dataFile.print(GyroY);
+                dataFile.print(", ");
+                dataFile.println(GyroZ);
+                dataFile.flush();
+                // dataFile.print(", ");
+                // dataFile.println(angle);
+            }
 
-        // 조건 확인 및 서보 모터 작동
-        if (isAngleCheckEnabled && (angle <= 65 || angle >= 110)) {
-            deployParachute();
+            // // 조건 확인 및 서보 모터 작동
+            // if (angle <= 65 || angle >= 110)
+            // {
+            //     deployParachute();
+            // }
+            // else if (AccZ <= abs(0.3))
+            // {
+            //     deployParachute();
+            // }
+            // while(1){
+            //     dt2 = millis()-loopStartTime;
+            //     if(200>=dt2){
+            //         Serial.print(dt2);
+            //         break;
+            //     }
+            //     delay(10);
+            // }
         }
-        else if (isAngleCheckEnabled && (AccZ <= abs(0.3))) {
-            deployParachute();
-        }
+        prevTime = loopStartTime;
     }
-    dt=loopStartTime-micros();
 }
 
 // 낙하산 사출하는 함수
-void deployParachute() { 
+void deployParachute()
+{
     servo.write(72);
     delay(1000);
     servo.write(0);
+    Serial.println("parachute timeout");
+    MsTimer2::stop();
 }
 
 // Angle계산하는 함수
-void AngleCalculator(float yaw, float pitch, float roll) {
+void AngleCalculator(float yaw, float pitch, float roll)
+{
     // 도(degree)에서 라디안(radian)으로 변환
     float yawRad = yaw * DEG_TO_RAD;
     float pitchRad = pitch * DEG_TO_RAD;
@@ -132,8 +178,13 @@ void AngleCalculator(float yaw, float pitch, float roll) {
     angle = acos(dotProduct / vectorMagnitude) * RAD_TO_DEG;
 }
 
-
-
-// void complementary_Filter(){
-
-// }
+void filter(float AccX, float AccY, float AccZ, float GyroX, float GyroY, float GyroZ)
+{
+    acc_Pitch = atan2(AccX, sqrt(AccY*AccY + AccZ*AccZ));
+    acc_Roll = atan2(AccY, sqrt(AccX*AccX + AccZ*AccZ));
+    gyro_Pitch = gyro_Pitch + GyroX * dt/1000.0f;
+    gyro_Roll = gyro_Roll + GyroX * dt/1000.0f;
+    gyro_Yaw = gyro_Yaw + GyroZ * dt/1000.0f;
+    filter_Pitch = 0.98 * (filter_Pitch + gyro_Pitch) + 0.02 * acc_Pitch;
+    filter_Roll = 0.98 * (filter_Roll + gyro_Roll) + 0.02 * acc_Roll;
+}
