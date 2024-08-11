f0f8d6f2dc30fddeca12ac33b22c4644a03c2e61
AutoCorrelation <minjun.chung@gmail.com>
2024-08-11 10:45:19 +0900

    zz

--- NewRocket/NewRocket.ino
+++ NewRocket/NewRocket.ino
@@ -14,11 +14,11 @@ MPU9250 mpu;
 Servo servo;
 File dataFile;
 float yaw, pitch, roll;
-float angle = 0.0;             // 전역 변수로 선언하여 AngleCalculator에서 사용
-unsigned long prevTime;        // 타이머를 위한 변수
-unsigned long loopStartTime;   // 타이머를 위한 변수
-float timeWhen;                // 타이머를 위한 변수
-const unsigned long dt = 173;  // 0.173 s=1730ms
+float angle = 0.0;            // 전역 변수로 선언하여 AngleCalculator에서 사용
+unsigned long prevTime;       // 타이머를 위한 변수
+unsigned long loopStartTime;  // 타이머를 위한 변수
+float timeWhen;               // 타이머를 위한 변수
+const unsigned long dt = 173; // 0.173 s=1730ms
 float acc_Pitch, gyro_Pitch = 0;
 float acc_Roll, gyro_Roll = 0;
 float gyro_Yaw = 0;
@@ -28,31 +28,36 @@ float GyroX, GyroY, GyroZ;
 // float Gyro[3]={0.0f};
 float filter_Pitch, filter_Roll;
 /*-------------------------------------*/
-float rocketVector[3] = { 0.0f };
-float z_Unitvector[3] = { 0.0f, 0.0f, 1.0f };
+float rocketVector[3] = {0.0f};
+float z_Unitvector[3] = {0.0f, 0.0f, 1.0f};
 float a, b, c = 0.0f;
 float SEq_1 = 1.0f, SEq_2 = 0.0f, SEq_3 = 0.0f, SEq_4 = 0.0f;
 float rocketAngle = 0.0f;
 float linearAcc[3];
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Wire.begin();
   servo.attach(servoPin);
   servo.write(0);
   delay(1000);
 
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect))
+  {
     while (1)
-      ;  // 초기화 실패 시 무한 루프
+      ; // 초기화 실패 시 무한 루프
   }
 
   dataFile = SD.open("data.txt", FILE_WRITE);
-  if (dataFile) {
+  if (dataFile)
+  {
     dataFile.println("Time:AccX,AccY,AccZ,GyroX,GyroY,GyroZ,Angle,linearAccX,linearAccY,linearAccZ");
-  } else {
+  }
+  else
+  {
     while (1)
-      ;  // 파일 열기 실패 시 무한 루프
+      ; // 파일 열기 실패 시 무한 루프
   }
 
   // MPU9250 초기 설정 최소화
@@ -66,9 +71,10 @@ void setup() {
   setting.accel_fchoice = 0x01;
   setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
 
-  if (!mpu.setup(0x68, setting)) {
+  if (!mpu.setup(0x68, setting))
+  {
     while (1)
-      ;  // MPU9250 초기화 실패 시 무한 루프
+      ; // MPU9250 초기화 실패 시 무한 루프
   }
   mpu.verbose(true);
   // mpu.calibrateMag();
@@ -80,15 +86,17 @@ void setup() {
   // startTime = millis(); // 설정 시작 시간을 기록
   // isAngleCheckEnabled = false; // 설정 완료 전까지 angle 체크 비활성화
 
-  MsTimer2::set(13000, deployParachute);  // time out 사출조건 - 1
+  MsTimer2::set(13000, deployParachute); // time out 사출조건 - 1
   MsTimer2::start();
 }
 
-void loop() {
+void loop()
+{
   loopStartTime = millis();
   // if (loopStartTime - prevTime >= dt)
   // {
-  if (mpu.update()) {
+  if (mpu.update())
+  {
     /*---------------------*/
     yaw = mpu.getYaw();
     pitch = mpu.getPitch();
@@ -98,7 +106,7 @@ void loop() {
     AccZ = mpu.getAccZ();
     GyroX = mpu.getGyroX();
     GyroY = mpu.getGyroY();
-    GyroZ = mpu.getGyroZ();  // data Value 추가하기
+    GyroZ = mpu.getGyroZ(); // data Value 추가하기
     SEq_1 = mpu.getQuaternionW();
     SEq_2 = mpu.getQuaternionX();
     SEq_3 = mpu.getQuaternionY();
@@ -107,54 +115,20 @@ void loop() {
     linearAcc[1] = mpu.getLinearAcc(1);
     linearAcc[2] = mpu.getLinearAcc(2);
     /*-------------------------------*/
-    timeWhen = millis() / 1000.0f;  // 시간 저장하기
+    timeWhen = millis() / 1000.0f; // 시간 저장하기
     // filter(AccX, AccY, AccZ, GyroX, GyroY, GyroZ);
-    rocketVector[0] = 2.0 * (SEq_2 * SEq_4 - SEq_1 * SEq_3);        // x축
-    rocketVector[1] = 2.0 * (SEq_1 * SEq_2 + SEq_3 * SEq_4);        // y component
-    rocketVector[2] = 1.0 - 2.0 * (SEq_2 * SEq_2 + SEq_3 * SEq_3);  // z component
+    rocketVector[0] = 2.0 * (SEq_2 * SEq_4 - SEq_1 * SEq_3);       // x축
+    rocketVector[1] = 2.0 * (SEq_1 * SEq_2 + SEq_3 * SEq_4);       // y component
+    rocketVector[2] = 1.0 - 2.0 * (SEq_2 * SEq_2 + SEq_3 * SEq_3); // z component
     a = rocketVector[0] * z_Unitvector[0] + rocketVector[1] * z_Unitvector[1] + rocketVector[2] * z_Unitvector[2];
     b = sqrt(rocketVector[0] * rocketVector[0] + rocketVector[1] * rocketVector[1] + rocketVector[2] * rocketVector[2]);
     c = sqrt(z_Unitvector[0] * z_Unitvector[0] + z_Unitvector[1] * z_Unitvector[1] + z_Unitvector[2] * z_Unitvector[2]);
 
-    rocketAngle = acos(a / (b * c)) * RAD_TO_DEG;  // inner product(dot product) Rocket vector with Z unit vector
-
-<<<<<<< HEAD
-        // Serial.println(rocketAngle); // test complete.
-        float temp = timeWhen - 1.59f;
-        if (dataFile)
-        {
-            char buffer[200];
-            sprintf(buffer,
-                    "%.2f, %.2f ,%.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f, %.2f",
-                    temp, AccX, AccY, AccZ, GyroX, GyroY, GyroZ, rocketAngle, linearAcc[0], linearAcc[1], linearAcc[2]);
-            dataFile.println(buffer);
-            // dataFile.print(timeWhen - 1.59f);
-            // dataFile.print(": ");
-            // dataFile.print(AccX);
-            // dataFile.print(", ");
-            // dataFile.print(AccY);
-            // dataFile.print(", ");
-            // dataFile.print(AccZ);
-            // dataFile.print(", ");
-            // dataFile.print(GyroX);
-            // dataFile.print(", ");
-            // dataFile.print(GyroY);
-            // dataFile.print(", ");
-            // dataFile.print(GyroZ);
-            // dataFile.print(", ");
-            // dataFile.print(rocketAngle);
-            // dataFile.print(", ");
-            // dataFile.print(linearAcc[0]);
-            // dataFile.print(", ");
-            // dataFile.print(linearAcc[1]);
-            // dataFile.print(", ");
-            // dataFile.println(linearAcc[2]);
-            dataFile.flush();
-        }
-=======
+    rocketAngle = acos(a / (b * c)) * RAD_TO_DEG; // inner product(dot product) Rocket vector with Z unit vector
     // Serial.println(rocketAngle); // test complete.
     float temp = timeWhen - 1.59f;
-    if (dataFile) {
+    if (dataFile)
+    {
       char b1[8];
       char b2[8];
       char b3[8];
@@ -212,11 +186,12 @@ void loop() {
   }
   prevTime = loopStartTime;
   // }
-  Serial.println(millis() - loopStartTime);  // time testing.
+  Serial.println(millis() - loopStartTime); // time testing.
 }
 
 // 낙하산 사출하는 함수
-void deployParachute() {
+void deployParachute()
+{
   servo.write(72);
   delay(1000);
   servo.write(0);
@@ -225,7 +200,8 @@ void deployParachute() {
 }
 
 // Angle계산하는 함수
-void AngleCalculator(float yaw, float pitch, float roll) {
+void AngleCalculator(float yaw, float pitch, float roll)
+{
   // 도(degree)에서 라디안(radian)으로 변환
   float yawRad = yaw * DEG_TO_RAD;
   float pitchRad = pitch * DEG_TO_RAD;
